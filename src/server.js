0c798ee4f9857e7f13dd283d8c1d27366901753b
Asdren JERLIU <114486010+asdreen@users.noreply.github.com>
2023-01-22 11:47:29 +0100

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import listEndpoints from "express-list-endpoints";
+import productRouter from "./api/products/index.js";
+import reviewRouter from "./api/reviews/index.js";
+import {
+  badRequestHandler,
+  genericServerErrorHandler,
+  notFoundHandler,
+  unauthorizedHandler,
+} from "./errorHandlers.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const server = express();
+const port = process.env.PORT || 3001;
+
+dotenv.config();
+
+server.use(cors());
+server.use(express.json());
+server.use("/products", productRouter);
+server.use("/reviews", reviewRouter);
+server.use(badRequestHandler);
+server.use(unauthorizedHandler);
+server.use(notFoundHandler);
+server.use(genericServerErrorHandler);
+
+mongoose.connect(process.env.MONGO_URL);
+
+mongoose.connection.on("connected", () => {
+  server.listen(port, () => {
+    console.log(
+      "server is connected to Database and is running on port:",
+      port
+    );
+    console.table(listEndpoints(server));
+  });
+});
